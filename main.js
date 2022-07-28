35249ab92d9c79bc75f0749cb92822e11cf403ba
Futa Ando <futa990628@gmail.com>
2022-07-28 19:34:32 +0900

    ポートフォリオサイトを更新

--- main.js
+++ main.js
@@ -138,3 +138,17 @@ onClickButton.addEventListener("mouseleave", function (event) {
   p1.style.display = "block";
   p2.style.display = "none";
 });
+
+// スクロール
+let fadeInTarget = document.querySelectorAll(".fade-in");
+window.addEventListener("scroll", () => {
+  for (let i = 0; i < fadeInTarget.length; i++) {
+    const rect = fadeInTarget[i].getBoundingClientRect().top;
+    const scroll = window.pageYOffset || document.documentElement.scrollTop;
+    const offset = rect + scroll;
+    const windowHeight = window.innerHeight; // 現在のブラウザの高さ
+    if (scroll > offset - windowHeight + 150) {
+      fadeInTarget[i].classList.add("scroll-in");
+    }
+  }
+});
